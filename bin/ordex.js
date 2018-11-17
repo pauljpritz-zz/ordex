fdd0c5d30c16a5c455569a5e32a7bd40d82c1c08
Daniel Perez <daniel@perez.sh>
2018-11-17 16:20:36 +0000

    Add eth-client

--- bin/ordex.js
+++ bin/ordex.js
@@ -2,11 +2,13 @@
 
 const createDB = require('../lib/db');
 const createAPI = require('../lib/api');
+const createW3 = require('../lib/eth-client');
 
 const port = 3000;
 
 createDB((db) => {
-  const api = createAPI(db);
+  const w3 = createW3();
+  const api = createAPI(db, w3);
   api.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
   });
