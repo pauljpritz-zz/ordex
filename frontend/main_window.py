4eaf0be25f94fa3031bbeefa87f19ba6a23e350d
Daniel Perez <daniel@perez.sh>
2018-11-18 03:08:52 +0000

    Update client

--- frontend/main_window.py
+++ frontend/main_window.py
@@ -1,15 +1,23 @@
 import sys
+from os import path
+
+import web3
 import websockets
-import asyncio
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QVBoxLayout, QHBoxLayout
-from PyQt5.QtWidgets import QLabel, QPushButton
-from PyQt5.QtCore import pyqtSlot
-
-
 import requests
+import asyncio
 
-ENDPOINT = "http://localhost:3000/offer"
 
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import QLabel, QPushButton, QComboBox
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QIntValidator
+
+
+
+ENDPOINT = "http://localhost:3000"
+W3_ENDPOINT = "http://127.0.0.1:7545"
+
+w3 = web3.Web3(web3.HTTPProvider(W3_ENDPOINT))
 
 async def connect(address, id):
     async with websockets.connect(address) as websocket:
@@ -18,53 +26,50 @@ async def connect(address, id):
         transaction = await websocket.recv()
 
 
+def get_tokens():
+    return requests.get(path.join(ENDPOINT, "tokens")).json()
+
 
 class App(QWidget):
-
     def __init__(self):
         super().__init__()
         self.title = 'Ordex Decentralized Exchange'
-        self.left = 10
-        self.top = 10
-        self.width = 840
-        self.height = 680
         self.layout = QVBoxLayout()
+        self.tokens = get_tokens()
+        self.account = w3.personal.listAccounts[0]
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setLayout(self.layout)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        self.setFixedSize(300, 150)
-        self.source = self.add_row("source")
-        self.target = self.add_row("target")
-        self.source_amount = self.add_row("source_amount")
-        self.target_amount = self.add_row("target_amount")
+        self.setGeometry(10, 10, 840, 680)
+        # self.setFixedSize(300, 150)
+        self.source = self.add_row("Source token", widget=self.token_combo_box())
+        self.target = self.add_row("Target token", widget=self.token_combo_box())
+        self.source_amount = self.add_row("Source amount")
+        self.source_amount.setValidator(QIntValidator(1, 1_000_000))
+        self.target_amount = self.add_row("Target amount")
+        self.target_amount.setValidator(QIntValidator(1, 1_000_000))
         button = QPushButton("Send")
         button.clicked.connect(self.handle_send)
         self.layout.addWidget(button)
-        # self.getSource()
-        # self.getTarget()
-        # self.getSourceAmount()
-        # self.getTargetAmount()
-        # self.getSignature()
 
         self.show()
 
     @pyqtSlot()
     def handle_send(self):
-        print(self.source.text())
-        print(self.target.text())
+        print(self.source.currentText())
+        print(self.target.currentText())
         print(self.source_amount.text())
         print(self.target_amount.text())
 
     def send_request(self):
-        r = requests.post(ENDPOINT, json=dict(
-            sourceAmount=self.source_amount.text(),
-            targetAmount=self.target_amount.text(),
-            sourceToken=self.source.text(),
-            targetToken=self.target.text(),
-            address=self.get_address()
+        r = requests.post(self._endpoint("offer"), json=dict(
+            sourceAmount=int(self.source_amount.text()),
+            targetAmount=int(self.target_amount.text()),
+            sourceToken=self.source.currentText(),
+            targetToken=self.target.currentText(),
+            address=self.account
         ))
         if r.status_code == 200:
             pass
@@ -77,14 +82,14 @@ class App(QWidget):
         # TODO: do something useful
         return "123467"
 
-    def add_row(self, label_text):
+    def add_row(self, label_text, widget=None):
         row_layout = QHBoxLayout()
         row_layout.addWidget(QLabel(label_text))
-        line_edit = QLineEdit()
-        row_layout.addWidget(line_edit)
+        if widget is None:
+            widget = QLineEdit()
+        row_layout.addWidget(widget)
         self.layout.addLayout(row_layout)
-        return line_edit
-
+        return widget
 
     def getSource(self):
         items = ("ETH", "BTC")
@@ -108,10 +113,19 @@ class App(QWidget):
         if okPressed:
             print(target_amount)
 
+    def token_combo_box(self):
+        box = QComboBox()
+        box.addItems(self.tokens)
+        return box
+
     def getSignature(self):
         text, okPressed = QInputDialog.getText(self, "User signature", "Signature:", QLineEdit.Normal, "")
         if okPressed and text != '':
             print(text)
+
+    @staticmethod
+    def _endpoint(route):
+        return path.join(ENDPOINT, route)
 
 
 if __name__ == '__main__':
